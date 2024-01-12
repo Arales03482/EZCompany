d770a2bad941161243ca2e69ee78b494e95e1381
Bacon <69711892+Arales03482@users.noreply.github.com>
2024-01-12 15:28:32 +0100

    Delete PluginInfo.cs

--- PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EZCompanyMod
-{
-    internal class PluginInfo
-    {
-        public const string GUID = "com.ARealModder.lethalcompany.ezcompany";
-        public const string Name = "EZCompany";
-        public const string Version = "1.0.0";
-    }
-}
